f46cb0552ebbbecedd7250f5fa2dbc233ad6075f
jorge apaza <jorgeapaza2124@gmail.com>
2026-08-28 18:18:39 -0300

    primer commit

---
+++ src/main/java/com/mycompany/poo_tp3_grupo6/Poo_tp3_grupo6.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.poo_tp3_grupo6;
+
+/**
+ *
+ * @author 54381
+ */
+public class Poo_tp3_grupo6 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
